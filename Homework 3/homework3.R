e30028a47c04bc8bce92cb23f6acdf93a226732b
Oliver Shanklin <oliver.shanklin@outlook.com>
2019-02-07 20:36:29 -0700

    added things for Homework 3

--- Homework 3/homework3.R
+++ Homework 3/homework3.R
@@ -1,11 +1,11 @@
 # Homework 3
 # STAT 421
-
+set.seed(4202019)
 x0 <- 0
 p <- .5
 q <- 1-p
 iter <- 200
-numChains <- 3
+numChains <- 500
 
 xmtx <- matrix(0,numChains,iter+1)
 
@@ -35,3 +35,13 @@ palette <- distinctColorPalette(numChains)
 for(i in seq(2, numChains)){
   lines(xmtx[i,], col = palette[i])
 }
+
+
+
+hist(xmtx[,50], freq =F, col = "lightblue")
+mean(xmtx[,50])
+var(xmtx[,50])
+
+hist(xmtx[,200], freq=F, col = "pink")
+mean(xmtx[,200])
+var(xmtx[,200])
